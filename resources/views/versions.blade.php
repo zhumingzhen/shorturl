9fa5f2fb142f40a7707f8be0b10c4f5ea1598f96
zhumingzhen <itdream53@gmail.com>
2017-11-19 11:58:13 +0800

    debug

--- resources/views/versions.blade.php
+++ resources/views/versions.blade.php
@@ -7,17 +7,19 @@
         <title>版本更新</title>
     </head>
     <body>
-        <div>
-            <h4>2017-10-13 v0.8</h4>
-            短链基础版可用(原生)
-        </div>
-        <div>
-            <h4>2017-11-11 v1.0</h4>
-            项目迁移(laravel)
-        </div>
-        <div>
-            <h4>2017-11-15 v1.1</h4>
-            项目迁移优化(laravel)
+        <div style="text-align: center">
+            <div>
+                <h4>2017-10-13 v0.8</h4>
+                短链基础版可用(原生)
+            </div>
+            <div>
+                <h4>2017-11-11 v1.0</h4>
+                项目迁移(laravel)
+            </div>
+            <div>
+                <h4>2017-11-15 v1.1</h4>
+                项目迁移优化(laravel)
+            </div>
         </div>
     </body>
 </html>
