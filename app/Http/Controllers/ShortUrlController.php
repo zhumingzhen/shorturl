cc14bd22cb5c54712012bb8866bc3bbc7e4036a9
zhumingzhen <itdream53@gmail.com>
2017-11-24 10:05:58 +0800

    test

--- app/Http/Controllers/ShortUrlController.php
+++ app/Http/Controllers/ShortUrlController.php
@@ -58,18 +58,18 @@ class ShortUrlController extends Controller
         $record = $reader->city('115.29.79.115');
 
 //        print($record->country->isoCode . "\n"); // 'US'
-        print($record->country->name . "\n"); // 'United States'
-        print($record->country->names['zh-CN'] . "\n"); // '美国'
-
-        print($record->mostSpecificSubdivision->name . "\n"); // 'Minnesota'
+//        print($record->country->name . "\n"); // 'United States'
+//        print($record->country->names['zh-CN'] . "\n"); // '美国'
+//        print($record->mostSpecificSubdivision->name . "\n"); // 'Minnesota'
 //        print($record->mostSpecificSubdivision->isoCode . "\n"); // 'MN'
-
-        print($record->city->name . "\n"); // 'Minneapolis'
-
+//        print($record->city->name . "\n"); // 'Minneapolis'
 //        print($record->postal->code . "\n"); // '55455'
-
 //        print($record->location->latitude . "\n"); // 44.9733
 //        print($record->location->longitude . "\n"); // -93.2323
+        
+        print($record->country->names['zh-CN'] . "\n"); // '美国'
+        print($record->mostSpecificSubdivision->name['zh-CN'] . "\n"); // 'Minnesota'
+        print($record->city->name['zh-CN'] . "\n"); // 'Minneapolis'
 
 
 
