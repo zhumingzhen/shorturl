07ad86ac39a40082718a140291729e3ffc9785fd
zhumingzhen <itdream53@gmail.com>
2017-11-24 16:38:44 +0800

    getos

--- app/Http/Controllers/ShortUrlController.php
+++ app/Http/Controllers/ShortUrlController.php
@@ -68,8 +68,10 @@ class ShortUrlController extends Controller
 //        $city = $this->findCityByIp($ip);  // 根据ip 获取城市 taobao 带运营商
 
         $browser = $this->getBrowser();  // 获取浏览器信息
-        $browser = $this->get_broswer();  // 获取浏览器信息
-        dd($browser);
+//        $browser = $this->get_broswer();  // 获取浏览器信息 带具体版本
+
+        $os = $this->get_os();  // 获取系统信息
+        dd($os);
 
 
 
@@ -91,6 +93,133 @@ class ShortUrlController extends Controller
         */
 
 
+    }
+
+    /**
+     * 获取客户端操作系统信息包括win10
+     * @param  null
+     * @author  itdrean<itdream53@gmail.com>
+     * @return string
+     */
+    public function get_os(){
+        $agent = $_SERVER['HTTP_USER_AGENT'];
+        $os = false;
+
+        if (preg_match('/win/i', $agent) && strpos($agent, '95'))
+        {
+            $os = 'Windows 95';
+        }
+        else if (preg_match('/win 9x/i', $agent) && strpos($agent, '4.90'))
+        {
+            $os = 'Windows ME';
+        }
+        else if (preg_match('/win/i', $agent) && preg_match('/98/i', $agent))
+        {
+            $os = 'Windows 98';
+        }
+        else if (preg_match('/win/i', $agent) && preg_match('/nt 6.0/i', $agent))
+        {
+            $os = 'Windows Vista';
+        }
+        else if (preg_match('/win/i', $agent) && preg_match('/nt 6.1/i', $agent))
+        {
+            $os = 'Windows 7';
+        }
+        else if (preg_match('/win/i', $agent) && preg_match('/nt 6.2/i', $agent))
+        {
+            $os = 'Windows 8';
+        }else if(preg_match('/win/i', $agent) && preg_match('/nt 10.0/i', $agent))
+        {
+            $os = 'Windows 10';#添加win10判断
+        }else if (preg_match('/win/i', $agent) && preg_match('/nt 5.1/i', $agent))
+        {
+            $os = 'Windows XP';
+        }
+        else if (preg_match('/win/i', $agent) && preg_match('/nt 5/i', $agent))
+        {
+            $os = 'Windows 2000';
+        }
+        else if (preg_match('/win/i', $agent) && preg_match('/nt/i', $agent))
+        {
+            $os = 'Windows NT';
+        }
+        else if (preg_match('/win/i', $agent) && preg_match('/32/i', $agent))
+        {
+            $os = 'Windows 32';
+        }
+        else if (preg_match('/linux/i', $agent))
+        {
+            $os = 'Linux';
+        }
+        else if (preg_match('/unix/i', $agent))
+        {
+            $os = 'Unix';
+        }
+        else if (preg_match('/sun/i', $agent) && preg_match('/os/i', $agent))
+        {
+            $os = 'SunOS';
+        }
+        else if (preg_match('/ibm/i', $agent) && preg_match('/os/i', $agent))
+        {
+            $os = 'IBM OS/2';
+        }
+        else if (preg_match('/Mac/i', $agent) && preg_match('/PC/i', $agent))
+        {
+            $os = 'Macintosh';
+        }
+        else if (preg_match('/PowerPC/i', $agent))
+        {
+            $os = 'PowerPC';
+        }
+        else if (preg_match('/AIX/i', $agent))
+        {
+            $os = 'AIX';
+        }
+        else if (preg_match('/HPUX/i', $agent))
+        {
+            $os = 'HPUX';
+        }
+        else if (preg_match('/NetBSD/i', $agent))
+        {
+            $os = 'NetBSD';
+        }
+        else if (preg_match('/BSD/i', $agent))
+        {
+            $os = 'BSD';
+        }
+        else if (preg_match('/OSF1/i', $agent))
+        {
+            $os = 'OSF1';
+        }
+        else if (preg_match('/IRIX/i', $agent))
+        {
+            $os = 'IRIX';
+        }
+        else if (preg_match('/FreeBSD/i', $agent))
+        {
+            $os = 'FreeBSD';
+        }
+        else if (preg_match('/teleport/i', $agent))
+        {
+            $os = 'teleport';
+        }
+        else if (preg_match('/flashget/i', $agent))
+        {
+            $os = 'flashget';
+        }
+        else if (preg_match('/webzip/i', $agent))
+        {
+            $os = 'webzip';
+        }
+        else if (preg_match('/offline/i', $agent))
+        {
+            $os = 'offline';
+        }
+        else
+        {
+            $os = '未知操作系统';
+        }
+        return $os;
     }
 
     /**
@@ -123,6 +252,10 @@ class ShortUrlController extends Controller
             return 'unknown';
     }
 
+    /**
+     * 获取浏览器信息带具体版本
+     * @return string
+     */
     public function get_broswer(){
         $sys = $_SERVER['HTTP_USER_AGENT'];  //获取用户代理字符串
         if (stripos($sys, "Firefox/") > 0) {
