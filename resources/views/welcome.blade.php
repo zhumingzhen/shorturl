1b4dafb6201402322c28bd40edfcc66d61455c3e
zhumingzhen <itdream53@gmail.com>
2017-12-02 23:15:41 +0800

    add record

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5.5 666
+                    Laravel 5.5 333
                 </div>
 
                 <div class="links">
