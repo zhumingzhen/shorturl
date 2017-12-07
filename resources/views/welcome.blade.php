45a4614becec236bec1e52033b52656b39618191
zhumingzhen <itdream53@gmail.com>
2017-12-07 23:53:43 +0800

    required messages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5.5 6
+                    Laravel 5.5 66
                 </div>
 
                 <div class="links">
