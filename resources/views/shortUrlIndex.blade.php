4e030d74daabc9d6f55e4adc7c8f012e089f46ed
zhumingzhen <itdream53@gmail.com>
2017-12-08 15:35:55 +0800

    add 404 page

--- resources/views/shortUrlIndex.blade.php
+++ resources/views/shortUrlIndex.blade.php
@@ -17,7 +17,7 @@
     <!-- 短链展示 -->
     <div class="s-result">
         <span class="s-url" id="sUrl">http://2dw.win/url</span>
-        <button class="copy-s-url copy" id="copySUrl" data-clipboard-text="http://2dw.win/s/url">复制短链</button>
+        <button class="copy-s-url copy" id="copySUrl" data-clipboard-text="http://2dw.win/url">复制短链</button>
     </div>
     <!-- 作者 -->
     <div class="author">
