bf8a7473eaa6e3e98b102f5340650f5135f9034a
zhumingzhen <itdream53@gmail.com>
2017-12-08 15:39:44 +0800

    add 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -79,15 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5.5 404
+                    您访问的页面走丢了...
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/">回首页</a>
                 </div>
             </div>
         </div>
