bebc507ee0b4efe58003b15de13f00d7c8b345cf
zhumingzhen <itdream53@gmail.com>
2017-12-18 22:41:10 +0800

    debug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5.5 666
+                    Laravel 5.5 6665
                 </div>
 
                 <div class="links">
